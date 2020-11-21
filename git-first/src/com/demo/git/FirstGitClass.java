d2340287a3eeeccbebb646d736d65b819565708a
admin <383397258@qq.com>
2020-11-21 14:59:18 +0800

    添加一个TEST方法

--- git-first/src/com/demo/git/FirstGitClass.java
+++ git-first/src/com/demo/git/FirstGitClass.java
@@ -4,4 +4,7 @@ public class FirstGitClass {
     public static void main(String[] args) {
         System.out.println("aaa");
     }
+    public void test(){
+        System.out.println("test");
+    }
 }
